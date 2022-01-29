0b6f339d1875ec619576d762d2ab7e59e20d2804
Ernesto Barbosa <jose.barbosa@cwi.com.br>
2022-01-29 16:56:02 -0300

    CI Browserstack

---
+++ config/wdio.conf.js
@@ -0,0 +1,18 @@
+const { bsConf } = require('./bs.conf');
+const { localConf } = require('./local.conf');
+
+require('dotenv').config()
+
+function getConfig(){
+    switch (process.env.ENVIRONMENT) {
+        case 'local': default:
+            return localConf
+        case 'browserstack':
+            return bsConf
+        case 'saucelabs':
+            return {}
+    }
+}
+
+
+exports.config = getConfig()
--- wdio.ios.conf.js
+++ wdio.ios.conf.js
@@ -3,24 +3,16 @@ const allure = require('allure-commandline')
 const video = require('wdio-video-reporter');
 
 exports.config = {
-    hostname: 'localhost',
-    port: 4723,
-    path: '/wd/hub',
-    // user: "lojaebac_gBJk0x",
-    // key: "dQfHqx13fxpsVg1Ns1Yc",
 
-    // services: ['appium'],
-    // services: ['browserstack'],
-    specs: [
-        './test/specs/**/*.spec.js'
-    ],
-    suites: {
-        products: [
-            './test/specs/products.spec.js',
-            './test/specs/search.spec.js'
-        ]
-    },
-    framework: 'mocha',
+    ///bs
+    services: ['browserstack'],
+    user: "lojaebac_gBJk0x",
+    key: "dQfHqx13fxpsVg1Ns1Yc",
+
+    //sauce
+    /// ???
+
+
     capabilities: [
         {
             "platformName": "iOS",
@@ -32,57 +24,5 @@ exports.config = {
             'newCommandTimeout': 240
           }
     ],
-    waitforTimeout: 20000,
-    mochaOpts: {
-        timeout: 300000
-    },
-    reporters: ['spec',
-        ['allure', {
-            outputDir: 'allure-results',
-            disableWebdriverStepsReporting: true,
-            disableWebdriverScreenshotsReporting: true,
-        }],
-        [video, {
-            saveAllVideos: true,       // If true, also saves videos for successful test cases
-            videoSlowdownMultiplier: 50, // Higher to get slower videos, lower for faster videos [Value 1-100]
-        }]
-    ],
-    onComplete: function () {
-        const reportError = new Error('Could not generate Allure report')
-        const generation = allure(['generate', 'allure-results', '--clean'])
-        return new Promise((resolve, reject) => {
-            const generationTimeout = setTimeout(
-                () => reject(reportError),
-                5000)
-
-            generation.on('exit', function (exitCode) {
-                clearTimeout(generationTimeout)
-
-                if (exitCode !== 0) {
-                    return reject(reportError)
-                }
-
-                console.log('Allure report successfully generated')
-                resolve()
-            })
-        })
-    },
-    afterStep: function (test, scenario, { error, duration, passed }) {
-        if(error) {
-            driver.takeScreenshot()
-        }
-    },
-    beforeSuite: async function(){
-        //verificar se o app ja esta instalado e executando
-        let state = await driver.queryAppState("br.art.ebaconline")
-        if(state !== 4){
-            await driver.launchApp()
-        }
-    },
-    afterSuite: async function(){
-        //fechar o app
-        await driver.closeApp()
-    },
-    maxInstances: 1
 
 }
